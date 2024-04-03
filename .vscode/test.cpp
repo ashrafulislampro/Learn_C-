73c6c9f2a0b2389cff1ee874a9c5b0d5fb25776b
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-03 11:54:48 +0600

    Start Learning C++ programming language

---
+++ .vscode/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "\"Ashraful" << " Islam\"" << endl;
+    cout << "\tNarsingdi\n";
+    cout << "013039888888\n";
+
+    getchar();
+}

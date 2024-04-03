25c387de8c71f1fbe7adb1d7a159ee1d7ddd5388
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-03 15:58:54 +0600

    bitwise operator usage

--- bitWiseOperator.cpp
+++ bitWiseOperator.cpp
@@ -11,11 +11,15 @@ int main(){
     int c = a & b;  
     int d = a | b;
     int e = a ^ b;
+    int l = a << 3;  // multiple 3 times a value with 2
+    int r = a >> 3; //  divide 3 times a value with 2
 
         
     cout << "Bitwise AND : " << c <<endl;   
     cout << "Bitwise OR : " << d <<endl;     
-cout << "Bitwise EXOR : " << e <<endl;   
+    cout << "Bitwise EXOR : " << e <<endl; 
+    cout << "Bitwise Shift Right : " << r <<endl; 
+    cout << "Bitwise Shift Left : " << l <<endl; 
 
     getchar();
 }

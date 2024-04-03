b615dbc7cb8f50d5280d1cb9eca00a951180de11
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-03 12:13:46 +0600

    string variable

---
+++ stringVal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    char name[17] = "Ashraful Islam";
+
+    cout << "My name is : " <<name << endl;
+    getchar();
+}

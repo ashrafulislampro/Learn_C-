76791d1c6186a026d39d3602b38dd31a2d923696
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-03 12:10:50 +0600

    keyword, variable and data types

---
+++ valKeyDataType.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    float num1 = 10.5, num2 = 20.508978686;
+    char ch; 
+    ch = 'a';
+
+    cout<< "Number1 = "<< num1 <<endl << "Number2 = "<< num2 <<endl;
+    cout << "Character = "<< ch;
+
+    getchar();
+}

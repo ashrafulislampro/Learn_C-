a1cf7228523dc7d57066ac0acb3684cc46d40ee6
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-04-13 15:20:41 +0600

    template Specialization usage

---
+++ templateSpecialization.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class MyClass{
+    public:
+        MyClass(T x){
+            cout << x << "- not a char " <<endl;
+        }
+};
+
+template < >
+class MyClass<char>{
+    public:
+        MyClass(char x){
+            cout <<x<<" is a char!"<<endl;
+        }
+};
+
+int main(){
+    MyClass<int> ob1(45);
+    MyClass<double> ob2(5.47);
+    MyClass<char> ob3('A');
+    
+    return 0;
+}
